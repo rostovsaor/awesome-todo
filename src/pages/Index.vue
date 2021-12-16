36d18aea81fd64483e4d6214aea6986841bde34e
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2021-12-16 14:51:30 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="" padding>
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+    <input v-model="message" type="text" />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      message: 'I love Vue.js'
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    }
+  }
+}
+</script>
